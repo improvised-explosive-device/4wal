4b856e6cd7c18b44f92d4e387b3847113300fc0e
improvised-explosive-device <dogmadubstep@gmail.com>
2021-01-10 04:06:27 +0100

    Added features: 1. get random board with '-r', filter board by worksafe status with '-r [nsfw/sfw]' 2. specifiy file extension(s) with '-e ext [ext..]' 3. ask user if they want to download anyway if no software to set wp exists 4. small changes in printout (formatting, print thread url, more verbose help) 5. wpgtk support

--- 4wal.py
+++ 4wal.py
@@ -1,3 +1,5 @@
+import random
+import argparse
 from argparse import ArgumentParser, SUPPRESS
 from pathlib import Path
 from random import randint, choice
@@ -9,7 +11,9 @@ from urllib import request
 
 
 def default_command():
-    if which("wal") is not None:
+    if which("wpg") is not None:
+        return "wpg -s &>/dev/null"
+    elif which("wal") is not None:
         return "wal -q -i"
     elif which("feh") is not None:
         return "feh --bg-fill"
@@ -19,6 +23,19 @@ def default_command():
         return "gsettings set org.gnome.desktop.background picture-uri"
     else:
         return False
+
+
+def get_random_board():
+    boardnums = list()
+    boards = get('https://a.4cdn.org/boards.json').json()
+    # gotta catch em all
+    for board in boards["boards"]:
+        # nsfw filter
+        if board['ws_board'] == 0 and "nsfw" in args.random:
+            boardnums.append(board['board'])
+        if board['ws_board'] == 1 and "sfw" in args.random:
+            boardnums.append(board['board'])
+    args.board = random.choice(boardnums)
 
 
 def get_random_thread():
@@ -34,42 +51,50 @@ def get_random_post(thread_post_num):
     title = res["posts"][0].get("sub")
 
     post = choice(res["posts"])
-    while not post.get("ext") or (post["w"] < int(args.min_res.split("x")[0]) or post["h"] < int(args.min_res.split("x")[1])) or post["ext"] not in args.extension:
+    while not post.get("ext") or (post["w"] < int(args.min_res.split("x")[0]) or post["h"] < int(args.min_res.split("x")[1])) or post['ext'] not in args.extension:
         post = choice(res["posts"])
 
     filename = (post["filename"] if args.filename == "user" else str(post["tim"])) + post["ext"]
 
     request.urlretrieve(f"https://i.4cdn.org/{args.board}/{str(post['tim']) + post['ext']}", "".join((str(args.path).rstrip("/"), "/")) + filename)
 
-    Popen(args.command + " " + f"'{''.join((str(args.path).rstrip('/'), '/')) + filename}'", shell=True).wait()
+    if args.command:
+        Popen(args.command + " " + f"'{''.join((str(args.path).rstrip('/'), '/')) + filename}'", shell=True).wait()
 
     if not args.quiet:
-        if title: print("\33[1m" + title + "\33[0m")
-        print(">> " + post["now"] + " No." + str(post["no"]))
-        print("   " + filename + " " + str(post["w"]) + "x" + str(post["h"]))
-        print("   saved to " + str(args.path))
+        if title: print("   \33[1m" + title + "\33[0m")
+        print("   https://boards.4chan.org/{}/thread/{}\n" .format(args.board, thread_post_num))
+        print(">> {} No.{}" .format(str(post["now"]), post["no"]))
+        print("   {} ({}x{})".format(filename, str(post["w"]), str(post["h"])))
+        print("   saved to {}" .format(str(args.path)))
         print("  \33[90m ██ \33[0m\33[91m ██ \33[0m\33[92m ██ \33[0m\33[93m ██ \33[0m"
               "\33[94m ██ \33[0m\33[95m ██ \33[0m\33[96m ██ \33[0m\33[97m ██  \33[0m")
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Set a random wallpaper from 4chan!", add_help=False)
-    parser.add_argument("-h", "--help", action="help", default=SUPPRESS, help="\b\b\b\bshow this help message and exit")
-    parser.add_argument("-v", "--version", action="version", version="2.0", help="\b\b\b\bshow program version and exit")
-    parser.add_argument("-b", "--board",  metavar="\b", default="wg", help="board to scrape for wallpaper (default: /wg/)")
-    parser.add_argument("-c", "--command", metavar="\b", default=default_command(), help="command to set wallpaper")
-    parser.add_argument("-m", "--min-res", metavar="\b", default="0x0", help="specify minimum resolution (e.g. 1920x1080)")
-    parser.add_argument("-f", "--filename", metavar="\b", choices=["user", "server"], default="user", help="save file with \33[1muser\33[0m or \33[1mserver\33[0m filename")
-    parser.add_argument("-e", "--extension", metavar="\b", default=[".jpg", ".jpeg", ".png"], nargs="+", help="filter by file extension (default: .jpg .jpeg .png)")
-    parser.add_argument("-p", "--path", metavar="\b", default=Path.cwd(), help=f"where to save wallpaper files (default: {Path.cwd()}/)")
-    parser.add_argument("-q", "--quiet", default=False, action="store_true", help="\b\b\b\bsilence all output")
+    parser = ArgumentParser(description="Set a random wallpaper from 4chan!", add_help=False, formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=70))
+    arg_filt = parser.add_argument_group(title='filter arguments', description='')
+    arg_info = parser.add_argument_group(title='information arguments', description='')
+    arg_info.add_argument("-h", "--help", action="help", default=SUPPRESS, help="show this help message and exit")
+    arg_info.add_argument("-v", "--version", action="version", version="2.0", help="show program version and exit")
+    arg_filt.add_argument("-b", "--board", metavar="<board>", default="wg", help="board to scrape for wallpaper (default: /wg/)")
+    arg_filt.add_argument("-c", "--command", metavar="<cmd>", default=default_command(), help=f"command to set wallpaper (default: {default_command()})")
+    arg_filt.add_argument("-m", "--min-res", metavar="<res>", default="0x0", help="specify minimum resolution (ex. 1920x1080)")
+    parser.add_argument("-f", "--filename", metavar="user/server", choices=["user", "server"], default="user", help="save file with \33[1muser\33[0m or \33[1mserver\33[0m filename")
+    arg_filt.add_argument("-e", "--extension", metavar="<ext>", default=[".jpg", ".jpeg", ".png"], nargs="+", help="specify file extension(s) (default: .jpg .jpeg .png)")
+    arg_filt.add_argument("-r", "--random", metavar="nsfw/sfw", action="store", const=["nsfw", "sfw"], nargs="?", help="choose board at random, filter by \33[1mnsfw\33[0m or \33[1msfw\33[0m")
+    parser.add_argument("-p", "--path", metavar="<path>", default=Path.cwd(), help=f"where to save wallpaper files (default: {Path.cwd()})")
+    parser.add_argument("-q", "--quiet", default=False, action="store_true", help="silence all output")
     args = parser.parse_args()
 
     if not args.command:
-        print("No program for setting wallpapers found. Please specify one with '-c'.")
-        exit(1)
+        if input("No program for setting wallpapers found. Please specify one with '-c'.\nDownload anyway? (y/n)") != "y":
+            exit(1)
 
     try:
+        print("\nFetching...", end="\r")
+        if args.random:
+            get_random_board()
         random_thread = get_random_thread()
         get_random_post(random_thread)
     except KeyboardInterrupt:
